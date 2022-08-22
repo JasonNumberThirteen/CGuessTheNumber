ef5066022506586c8234df7fb145004a47f8464a
Jason <numberthirteen4026@gmail.com>
2022-08-22 13:54:17 +0200

    Added validation of numbers range

--- main.c
+++ main.c
@@ -5,7 +5,20 @@
 
 int main(void)
 {
-	printf("Hello World!\n");
+	printf("Guess the Number\n");
+
+	if(MIN_NUMBER > MAX_NUMBER)
+	{
+		printf("Incorrect range of numbers! Minimum is higher than maximum!\nProgram will now exit\n");
+
+		return -1;
+	}
+	else if(MIN_NUMBER == MAX_NUMBER)
+	{
+		printf("Incorrect range of numbers! Minimum is equal to maximum!\nProgram will now exit\n");
+
+		return -2;
+	}
 
 	return 0;
 }
