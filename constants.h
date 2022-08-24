4d5426cca2ae5b2940b488d2b222162887d490a5
Jason <numberthirteen4026@gmail.com>
2022-08-24 10:43:04 +0200

    Added a header file for constants

---
+++ constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_INCLUDED
+#define CONSTANTS_INCLUDED
+
+#define MIN_NUMBER 1
+#define MAX_NUMBER 30
+#define GAME_TITLE "Guess the Number"
+#define NEW_LINE_CHARACTER '\n'
+#define PROGRAM_EXIT_MESSAGE "Program will now exit"
+#define INCORRECT_RANGE_MESSAGE "Incorrect range of numbers!"
+
+#endif
--- main.c
+++ main.c
@@ -1,13 +1,7 @@
-#define MIN_NUMBER 1
-#define MAX_NUMBER 30
-#define GAME_TITLE "Guess the Number"
-#define NEW_LINE_CHARACTER '\n'
-#define PROGRAM_EXIT_MESSAGE "Program will now exit"
-#define INCORRECT_RANGE_MESSAGE "Incorrect range of numbers!"
-
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "constants.h"
 
 void validateRange(void);
 void loop(void);
